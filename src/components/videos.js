598f4fec64ebed4e9a81a3a93ca3425090b23538
Md-Hafizur-Rahman <hafizur15-1979@diu.edu.bd>
2022-09-30 20:27:05 +0600

    connet with firebase data base and showing video

--- src/components/videos.js
+++ src/components/videos.js
@@ -1,15 +1,39 @@
-import classes from "../styles/Vdeos.module.css";
+import { useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { Link } from "react-router-dom";
+import useVideoList from "../Hooks/useVideoList";
 import Video from "./Video";
+
 export default function Videos() {
+  const [page, setPage] = useState(1);
+  const { loading, error, videos, hasMore } = useVideoList(page);
+
   return (
-    <div className={classes.videos}>
-      <Video />
-      <Video />
-      <Video />
-      <Video />
-      <Video />
-      <Video />
-      <Video />
+    <div>
+      {videos.length > 0 && (
+        <InfiniteScroll
+          dataLength={videos.length}
+          hasMore={hasMore}
+          next={() => setPage(page + 2)}
+        >
+          {videos.map((video) =>
+            video.noq > 0 ? (
+              <Link to="/quiz" key={video.youtubeID}>
+                <Video
+                  title={video.title}
+                  id={video.youtubeID}
+                  noq={video.noq}
+                />
+              </Link>
+            ) : (
+              <Video title={video.title} id={video.youtubeID} noq={video.noq} />
+            )
+          )}
+        </InfiniteScroll>
+      )}
+      {!loading && videos.length === 0 && <div>No data found!</div>}
+      {error && <div>There was an error!</div>}
+      {loading && <div>Loading...</div>}
     </div>
   );
 }
