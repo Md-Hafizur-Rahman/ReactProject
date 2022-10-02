b4dd3040ebd7fc2698ee0a78587fdb83198319df
Md-Hafizur-Rahman <hafizur15-1979@diu.edu.bd>
2022-10-02 16:41:57 +0600

    Completed

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+const app = initializeApp({
+  apiKey: "AIzaSyB5Z8_XLgAvTuy4YD5OCFANqVtQe_aRqJI",
+  authDomain: "quizaplication-54a00.firebaseapp.com",
+  databaseURL:
+    "https://quizaplication-54a00-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "quizaplication-54a00",
+  storageBucket: "quizaplication-54a00.appspot.com",
+  messagingSenderId: "536272328634",
+  appId: "1:536272328634:web:019fba7fedd88f8c64ef83",
+  measurementId: "G-N49PNC94MG",
+  PexelsApiKey: "563492ad6f9170000100000184d88d7167cc4911b723233f31268259",
+});
+export default app;
